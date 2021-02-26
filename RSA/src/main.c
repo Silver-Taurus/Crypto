43d3c2ed75e23932b9ae533dcb6b96fa623000e8
Silver Taurus <kumar.ayush101@gmail.com>
2021-02-26 04:48:13 -0500

    Src completed

--- RSA/src/main.c
+++ RSA/src/main.c
@@ -1,8 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../include/rsa.h"
+#include "../include/primes.h"
 
-int main() {
-    char prime = 0;
+int main(int ac, char **av)
+{
+    long p, q;
 
-    printf("%d", prime);
+    //Seed for random
+    srandom(time(NULL));
+    printf("This program can accept 3 paramaters or no paramaters.\n\n");
+    printf("To use:\n%s <p> <q> <limit>\n%s\n\n", av[0], av[0]);
+    printf("Paramaters: p and q for RSA keygen, limit is to limit the Sieve. \n");
+    printf("If no paramaters are given random values will be used for p and q,\n");
+    printf("and the Sieve will default to a defined value.\n\nStarting...\n\n");
+    if (ac >= 4)
+    {
+        p = atol(av[1]);
+        q = atol(av[2]);
+        //Generate a list of prime numbers, used by keygen
+        sieve_of_eratosthenes(atol(av[3]));
+    }
+    else
+    {
+        sieve_of_eratosthenes(RSA_SIEVE_LIMIT);
+
+        //to generate p and q
+        p = gen_prime_list.primes[random() % gen_prime_list.length];
+        q = gen_prime_list.primes[random() % gen_prime_list.length];
+    }
+    printf("P used: %ld, Q used: %ld\n", p, q);
+
+    //Generate RSA keys
+    t_rsa results = rsa_keygen(p, q);
+
+    //Output Results
+    printf("P: %ld\nQ: %ld\nN: %ld\nphi(N): %ld\nChosen e: %ld\nd: %ld\n",
+           p, q, results.n, results.phi, results.e, results.d);
+
+    if (gen_prime_list.length != 0)
+        free(gen_prime_list.primes);
     return 0;
 }
---
+++ RSA/src/rsa.c
@@ -0,0 +1,45 @@
+#include "modulars.h"
+#include "primes.h"
+#include "rsa.h"
+
+static long	pick_e(long phi)
+{
+  //Ensure a list of pirmes has been generated
+  //if length is 0, user didn't pass paramaters, use defined limit
+  if (gen_prime_list.length == 0)
+    sieve_of_eratosthenes(RSA_SIEVE_LIMIT);
+
+  for (long i = 0; i < gen_prime_list.length; i++)
+    {
+      //This will simply pick the first, not a great idea,
+      //but simple to understand algo
+      if (gen_prime_list.primes[i] % phi != 0 && gcd(gen_prime_list.primes[i], phi) == 1)
+	return gen_prime_list.primes[i];
+    }
+
+  //Value must be greater than 1, so this is sufficent to indicate failure
+  return 0;
+}
+
+long		rsa_encrypt(long msg, long e, long n)
+{
+  return right_to_left(msg, e, n);
+}
+
+long		rsa_decrypt(long cyphertext, long d, long n)
+{
+  return right_to_left(cyphertext, d, n);
+}
+
+t_rsa		rsa_keygen(long p, long q)
+{
+  t_rsa		ret = { 0 };
+
+  //Ensure that n >= 2**b where b is the number of bits per block
+  //Example if encrypting a char, 2**8 = 256, thus n >= 256
+  ret.n = p * q;
+  ret.phi = (p - 1) * (q - 1);
+  ret.e = pick_e(ret.phi);
+  ret.d = modular_inverse(ret.e, ret.phi);
+  return ret;
+}
