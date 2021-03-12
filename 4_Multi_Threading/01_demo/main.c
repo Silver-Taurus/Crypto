1699afcc5927c10e69d7d1452a8b728e1adfc79e
Silver Taurus <kumar.ayush101@gmail.com>
2021-03-12 05:44:10 -0500

    Updated

---
+++ 4_Multi_Threading/01_demo/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+/*
+    A normal C function that is executed as a thread,
+    when its name is specified in 
+*/
